d8bd8dfca19e6e9918081e8242bdf19c655b8dc3
Christian de Abreu Borges <christianborges27@hotmail.com>
2024-05-25 06:50:52 -0300

    ajustes

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -30,7 +30,7 @@ public class Calculadora {
  */
 
 public int somaMultiplica (int n, int x, String m){
-    int r = 0; // r é igual ao resultado
+    int r ; // r é igual ao resultado
     if (m == "M"){ // M= multiplicação
         r = n * x;
     }else{
