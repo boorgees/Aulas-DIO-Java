0525871bcff8f49af20c0deee961368b1bf56456
Christian Borges <christianborges27@hotmail.com>
2024-05-28 22:07:36 -0300

    condicionais compostas

--- controle-fluxo/src/Notas.java
+++ controle-fluxo/src/Notas.java
@@ -1,11 +1,14 @@
 public class Notas {
     public static void main(String[] args) {
-        int nota = 10;
+        int nota = 7;
 
         if(nota >= 7) {
             System.out.println("Aprovado.");
-        
-        } else {
+            
+            } else if (nota >= 5 && nota < 7) {
+            System.out.println("Em recuperação.");
+           
+            } else {
             System.out.println("Reprovado.");
         }
     }
